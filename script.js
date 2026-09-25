27f5e9307c273315340debaf1396ebc897f7f629
coder123454321 <115930973+coder123454321@users.noreply.github.com>
2026-09-26 00:27:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-//your code here!
-const infi_list=document.getElementById("infi-list");
+const infi_list = document.getElementById("infi-list");
+
 function creat_List() {
     const li = document.createElement("li");
     li.innerText = "List Item";
     infi_list.appendChild(li);
 }
 
-// Initial 10
+// Initial 10 items
 for (let i = 0; i < 10; i++) {
     creat_List();
 }
-}
-creat_List(10);
-window.addEventListener("scroll",()=>{
-	if(window.innerHeight + window.scrollY>=document.documentElement.scrollHeight){
-		
-	creat_List();
-	}
-})
 
+window.addEventListener("scroll", () => {
+
+    if (
+        window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight
+    ) {
+        creat_List();
+        creat_List();
+    }
+
+});
