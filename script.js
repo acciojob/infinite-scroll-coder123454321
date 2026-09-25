85451188f17f30b26552e73ca04ef6bbdb041520
coder123454321 <115930973+coder123454321@users.noreply.github.com>
2026-09-26 00:20:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+const infi_list=document.getElementById("infi-list");
+function creat_List(){
+	infi_list.innerHTML+=`
+	<li>hello</li>
+	<li>bro</li>
+	`
+}
+creat_List();
+creat_List();
+creat_List();
+creat_List();
+creat_List();
+window.addEventListener("scroll",()=>{
+	if(window.innerHeight + window.scrollY>=document.documentElement.scrollHeight){
+		
+	creat_List();
+	}
+})
 
